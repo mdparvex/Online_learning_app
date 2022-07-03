d3f9d1be11ac50bf93aac41b2e0bcfb2e72905aa
mdparvex <mdparvex443@gmail.com>
2022-07-03 13:53:07 +0600

    database updated in online course app models.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+
+class ChoiceInline(admin.TabularInline):
+    model= Choice
+    extra = 3
+
+class QuestionInline(admin.ModelAdmin):
+    model = Question
+    inlines = [ChoiceInline]
 
 
 class LessonInline(admin.StackedInline):
@@ -28,3 +37,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
